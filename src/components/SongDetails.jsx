9285d0d8fbe34cb11a706541f9f6c81d460cb9f0
Alessio <alexeimv91@gmail.com>
2024-05-12 01:23:31 -0400

    Aceptar otros cambios

--- src/components/SongDetails.jsx
+++ src/components/SongDetails.jsx
@@ -2,7 +2,7 @@ import Message from "./Mensaje";
 import SongArtist from "./SongArtist";
 import SongLyric from "./SongLyric";
 
-//Comentario de prueba para merging ramas. Mas edicion de comentarios
+//Voy a poner otra cosa
 const SongDetails = ({ search, lyric, bio }) => {
   if (!lyric || !bio) return null;
 
